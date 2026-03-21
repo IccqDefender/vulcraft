60d2f429f76ea8b8d8804312bae897a047094fa1
sterben <iccq1221@gmail.com>
2026-03-21 23:53:08 +0700

    UPD: Add CreateLogicalDevice

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -13,7 +13,7 @@ void Engine::Run() {
 
     glfwMakeContextCurrent( _windowManager->window ); // MAKE CONTEXT WINDOW
 
-    InitVulkan();
+    InitVulkan( _windowManager );
 
     /* MAIN LOOP */
     MainLoop(_windowManager);
@@ -39,8 +39,11 @@ void Engine::InitGLFW() {
 }
 
 /* INIT VULKAN */
-void Engine::InitVulkan() {
+void Engine::InitVulkan(WindowManager* windowManager ) {
     VulkanInstantiate();
+    CreateSurface( windowManager );
+    PickPhysicalDevice();
+    //CreateLogicalDevice();
 }
 
 /* VULKAN INSTANTIATE */
@@ -74,9 +77,88 @@ void Engine::VulkanInstantiate() {
 }
 
 void Engine::CreateSurface( WindowManager *windowManager ) {
-    if (glfwCreateWindowSurface(m_instance, windowManager->window, nullptr, nullptr) != VK_SUCCESS) {
+    if (glfwCreateWindowSurface(m_instance, windowManager->window, nullptr, &m_surface) != VK_SUCCESS) {
         throw std::runtime_error("Failed to create Vulkan surface");
     }
+}
+
+void Engine::PickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(m_instance, &deviceCount, nullptr);
+    if (deviceCount == 0) {
+        throw std::runtime_error("Failed to find GPU's with Vulkan support");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(m_instance, &deviceCount, devices.data());
+
+    for (const auto& device : devices) {
+        VkPhysicalDeviceProperties m_deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &m_deviceProperties);
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        bool foundGraphics = false;
+        for (uint32_t i = 0; i < queueFamilyCount; i++) {
+            if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                foundGraphics = true;
+                break;
+            }
+        }
+        if (foundGraphics) {
+            m_physicalDevice = device;
+            break;
+        }
+    }
+
+    if (m_physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+}
+
+void Engine::CreateLogicalDevice() {
+    uint32_t m_queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevice, &m_queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(m_queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevice, &m_queueFamilyCount, queueFamilies.data());
+
+    uint32_t m_graphicsQueueFamily = 0;
+    for (uint32_t i = 0; i < m_queueFamilyCount; i++) {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            m_graphicsQueueFamily = i;
+            break;
+        }
+    }
+
+    float m_queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = m_queueFamilyCount;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &m_queuePriority;
+
+    VkPhysicalDeviceFeatures m_deviceFeatures{};
+
+    VkDeviceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    createInfo.pQueueCreateInfos = &queueCreateInfo;
+    createInfo.queueCreateInfoCount = 1;
+    createInfo.pEnabledFeatures = &m_deviceFeatures;
+
+    const char* deviceExtensions[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+    createInfo.enabledExtensionCount = 1;
+    createInfo.ppEnabledExtensionNames = deviceExtensions;
+
+    if (vkCreateDevice(m_physicalDevice, &createInfo, nullptr, &m_device) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create logical device!");
+    }
+
+    vkGetDeviceQueue(m_device, m_graphicsQueueFamily, 0, &m_graphicsQueue);
 }
 
 /* CLEANUP */
--- src/Engine.h
+++ src/Engine.h
@@ -8,24 +8,32 @@
 #include <vulkan/vulkan.h>
 #include "Managers/WindowManager/WindowManager.h"
 
+#include <vector>
+#include <iostream>
+
 class Engine {
 
     public:
         void Run();
 
     private:
+        VkInstance m_instance = nullptr;
+        VkSurfaceKHR m_surface = nullptr;
+        VkPhysicalDevice m_physicalDevice = nullptr;
+        VkDevice m_device = nullptr;
+        VkQueue m_graphicsQueue = nullptr;
+
         void MainLoop(WindowManager* windowManager);
         void Cleanup(WindowManager* windowManager);
 
         void InitGLFW();
 
-        void InitVulkan();
+        void InitVulkan(WindowManager* windowManager);
         void VulkanInstantiate();
 
         void CreateSurface(WindowManager* windowManager);
-
-        VkInstance m_instance;
-        VkSurfaceKHR m_surface;
+        void PickPhysicalDevice();
+        void CreateLogicalDevice();
 
 };
 
