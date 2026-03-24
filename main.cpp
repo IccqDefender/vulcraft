50f253fa7cbf2242bac3be13c4155b851877e8f9
sterben <iccq1221@gmail.com>
2026-03-24 17:02:40 +0700

    UPD: Create and compile shaders

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@
 #include <set>
 #include <fstream>
 
+VkShaderModule createShaderModule(VkDevice device, const std::vector<char>& code) {
+    VkShaderModuleCreateInfo shaderModuleCreateInfo = {};
+    shaderModuleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    shaderModuleCreateInfo.codeSize = code.size();
+    shaderModuleCreateInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+    VkShaderModule shaderModule;
+    vkCreateShaderModule(device, &shaderModuleCreateInfo, nullptr, &shaderModule);
+
+    return shaderModule;
+}
+
 std::vector<char> readFile(std::string filePath) {
     std::ifstream file(filePath, std::ios::ate | std::ios::binary);
 
@@ -252,6 +264,9 @@ int main() {
 
     std::vector<char> vertShaderCode = readFile("../shaders/vert.spv");
     std::vector<char> fragShaderCode = readFile("../shaders/frag.spv");
+
+    VkShaderModule vertShaderModule = createShaderModule(device, vertShaderCode);
+    VkShaderModule fragShaderModule = createShaderModule(device, fragShaderCode);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
