636565b9e19ea91e450d1a6d0095b9445bcfb376
sterben <iccq1221@gmail.com>
2026-04-14 00:04:47 +0700

    UPD: Create Vulkan DebugManager

--- src/renderer/vulkan/VulkanContext.cpp
+++ src/renderer/vulkan/VulkanContext.cpp
@@ -3,52 +3,148 @@
 #include "../../utils/Debug.h"
 #include "../../utils/Logger.h"
 
+static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT severity,
+    VkDebugUtilsMessageTypeFlagsEXT type,
+    const VkDebugUtilsMessengerCallbackDataEXT* callbackData,
+    void* userData)
+{
+    (void)type;
+    (void)userData;
+
+    if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
+        LOG_ERROR(Logger::Tag::Vulkan, callbackData->pMessage);
+    }
+    else if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+        LOG_WARN(Logger::Tag::Vulkan, callbackData->pMessage);
+    }
+    else if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) {
+        LOG_INFO(Logger::Tag::Vulkan, callbackData->pMessage);
+    }
+    else {
+        LOG_DEBUG(Logger::Tag::Vulkan, callbackData->pMessage);
+    }
+
+    return VK_FALSE;
+}
+
 void VulkanContext::Init(GLFWwindow *window) {
     m_window = window;
 
-    LOG_INFO(Logger::Tag::Vulkan, "Create Instance..");
+    LOG_INFO(Logger::Tag::Vulkan, "Create Vulkan Instance..");
     CreateInstance();
+
+    LOG_INFO(Logger::Tag::Vulkan, "Setting up Debug Manager..");
+    SetupDebugManager();
 }
 
 void VulkanContext::Cleanup() {
+    DestroyDebugManager();
+
+    if (m_instance != VK_NULL_HANDLE) {
+        vkDestroyInstance(m_instance, nullptr);
+        m_instance = VK_NULL_HANDLE;
+    }
 }
 
 void VulkanContext::CreateInstance() {
-    VkApplicationInfo appInfo = {};
+    VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "vulcraft";
+    appInfo.pApplicationName = "Vulcraft";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
+    appInfo.pEngineName = "Vulcraft Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_4;
+    appInfo.apiVersion = VK_API_VERSION_1_2;
 
-    VkInstanceCreateInfo createInfo = {};
+    VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
+    // GLFW extensions
     uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
+    const char** glfwExtensions =
+        glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char*> extensions(
+        glfwExtensions,
+        glfwExtensions + glfwExtensionCount
+    );
 
-    std::vector<const char*> requiredExtensions;
+    extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
-    for(uint32_t i = 0; i < glfwExtensionCount; i++) {
-        requiredExtensions.emplace_back(glfwExtensions[i]);
-    }
-
-    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-
+#ifdef VK_USE_PLATFORM_MACOS_MVK
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
     createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
 
-    createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
-    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    createInfo.enabledExtensionCount = (uint32_t)extensions.size();
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    // Validation layers
+    std::vector<const char*> layers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
 
-    createInfo.enabledLayerCount = 0;
+    createInfo.enabledLayerCount = (uint32_t)layers.size();
+    createInfo.ppEnabledLayerNames = layers.data();
+
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    debugCreateInfo.sType =
+        VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+    debugCreateInfo.messageSeverity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+    debugCreateInfo.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+    debugCreateInfo.pfnUserCallback = DebugCallback;
+
+    createInfo.pNext = &debugCreateInfo;
 
     VK_CHECK(vkCreateInstance(&createInfo, nullptr, &m_instance));
 
+}
+
+void VulkanContext::SetupDebugManager() {
+    if (m_instance == VK_NULL_HANDLE) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+    createInfo.messageSeverity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+    createInfo.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+    createInfo.pfnUserCallback = DebugCallback;
+
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)
+        vkGetInstanceProcAddr(m_instance, "vkCreateDebugUtilsMessengerEXT");
+
+    if (func) {
+        VK_CHECK(func(m_instance, &createInfo, nullptr, &m_debugMessenger));
+    }
+}
+
+void VulkanContext::DestroyDebugManager() {
+    if (m_debugMessenger == VK_NULL_HANDLE) return;
+
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)
+        vkGetInstanceProcAddr(m_instance, "vkDestroyDebugUtilsMessengerEXT");
+
+    if (func) {
+        func(m_instance, m_debugMessenger, nullptr);
+    }
+
+    m_debugMessenger = VK_NULL_HANDLE;
 }
--- src/renderer/vulkan/VulkanContext.h
+++ src/renderer/vulkan/VulkanContext.h
@@ -3,7 +3,6 @@
 
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
-
 #include <vector>
 
 class VulkanContext {
@@ -12,10 +11,13 @@ public:
     void Cleanup();
 private:
     void CreateInstance();
+    void SetupDebugManager();
+    void DestroyDebugManager();
 private:
-    VkInstance m_instance = VK_NULL_HANDLE;
     GLFWwindow* m_window = nullptr;
 
+    VkInstance m_instance = VK_NULL_HANDLE;
+    VkDebugUtilsMessengerEXT m_debugMessenger = VK_NULL_HANDLE;
 };
 
 #endif //VULCRAFT_VULKANCONTEXT_H
