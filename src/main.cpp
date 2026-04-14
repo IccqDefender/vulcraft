081d246a60fae51cef6785d1b795a2f9b28e8663
sterben <iccq1221@gmail.com>
2026-04-14 11:46:52 +0700

    UPD: Delete try catch from main

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,9 @@
 int main() {
     auto const _vk = std::make_unique<vk>();
 
-    try {
-        _vk->Run();
+    _vk->Run();
 
-        _vk->Stop();
-
-    }catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    _vk->Stop();
 
     return 0;
 }
